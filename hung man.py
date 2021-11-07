a8cefea399905196775d2ba3176e8d48ac0e8b79
Alirezaghorbi11 <92166188+Alirezaghorbi11@users.noreply.github.com>
2021-11-07 15:31:38 +0330

    Update hung man.py

--- hung man.py
+++ hung man.py
@@ -4,20 +4,13 @@ from os import system
 import random
 
 
-word_bank_clothes = ['cardigan' , 'coat' , 'jacket' , 'jeans' , 'top' , 'pants' , 'sweater' , 'skirt' , 'warm up suit' , 'hat']
-word_bank_animals = ['bee' , 'fly' , 'mosquito' , 'spider' , 'chicken' , 'bird' , 'bear' , 'camel', 'sheep' , 'jellyfish']
-word_bank_cities = ['mashhad' , 'abadan' , 'ankara' , 'berlin' , 'tehran' , 'doshanbe' , 'london' , 'torento', 'tabriz' , 'sanandaj']
-word_bank_computer = ['keyboard' , 'computer' , 'ram' , 'rom' , 'gate' , 'ssd' , 'hdd' , 'nvme', 'monitor' , 'battery']
-word_bank_football = ['chelsea' , 'arsenal' , 'manchester' , 'psg' , 'bayern' , 'davarcelona' , 'juv' , 'zenit', 'padide' , 'everton']
-word_bank_sport = ['baseball' , 'basketball' , 'boxin' , 'cycling' , 'golf' , 'volleyball' , 'tennis' , 'soccer', 'skiing' , 'rugby']
-word_bank_actor = ['tom hanks' , 'keanu reeves' , 'leonardo dicaprio' , 'will smith' , 'dwayne johnson' , 'tom cruise' , 'sxarlett johansson' , 'mark ruffalo', 'ben affleck' , 'margot robbie']
-word_bank_fruits = ['apple' , 'banana' , 'kiwi' , 'peach' , 'guava' , 'orange' , 'berry' , 'cherry', 'litchi' , 'melon']
+word_bank = ['apple' , 'chelsea' , 'sowp' , 'bitcoin' , 'top' , 'printer' , 'sxp' , 'skirt' , 'cardano' , 'alireza'] 
+
 
 
 array_choose = []
-t_array = []
 counter_win = 1
-joon = ['💚','💚','💚','💚','💚']
+life = ['💕','💕','💕','💕','💕']
 
 
 while True :
@@ -29,49 +22,21 @@ while True :
     menu_1 = input('\n\tPlease Choose one of them : ')
     os.system("clear")
     if menu_1 == '1' :
-        print('╒══════════════════════════════════════════════╕')
-        print('│              ⟹  ➊ - Clothes                  │')
-        print('│              ⟹  ➋ - Animals                  │')
-        print('│              ⟹  ➌ - Cities                   │')
-        print('│              ⟹  ➍ - Computer                 │')
-        print('│              ⟹  ➎ - Football                 │')
-        print('│              ⟹  ➏ - Sport                    │')
-        print('│              ⟹  ➐ - Actors                   │')
-        print('│              ⟹  ➑ - Ftuits                   │')
-        print('╘══════════════════════════════════════════════╛\n')
-        Contents_choice = input('plz choose one of the subjects(Enter number...) : \n\t ==> ')
-        match Contents_choice :
-            case '1' : 
-                t_array = word_bank_clothes
-            case '2' :
-                t_array = word_bank_animals
-            case '3' :
-                t_array = word_bank_cities
-            case '4' :
-                t_array = word_bank_computer
-            case '5' :
-                t_array = word_bank_football
-            case '6' :
-                t_array = word_bank_sport
-            case '7' :
-                t_array = word_bank_actor
-            case '8' :
-                t_array = word_bank_fruits
-
         user_true_char = []
         counter_win = 0
-        joon = ['💚','💚','💚','💚','💚']
-        word = random.choice(t_array)
+        life = ['💕','💕','💕','💕','💕']
+        word = random.choice(word_bank)
         if word in array_choose :
             while word in array_choose :
-                word = random.choice(t_array)
+                word = random.choice(word_bank)
         array_choose.append(word)
+
         while True:
             os.system("clear")
             print('\n╒════════════════════════════╕')
             print('│',end='')
-            for i in range (len(joon)) :
-                print('  ' ,joon[i],end='')
+            for i in range (len(life)) :
+                print('  ' ,life[i],end='')
             print('   │                           ',end='')
             
             for i in range(len(word)):
@@ -84,29 +49,31 @@ while True :
                     print('-' , end='')
             
             print('\n╘════════════════════════════╛\n')
-            if joon != [] and counter_win < len(word) :
+            if life != [] and counter_win < len(word)
+ :
                 user_char = input('\nPlz enter a char :  ')
                 if user_char in word:
                     print('Yes')
                     user_true_char.append(user_char)
                     counter_win +=1
                 else:
-                    joon.pop()
+                    life.pop()
                     print ('No')
 
-            if joon == []:
+            if life == []:
                 os.system("clear")
                 print('\n╒════════════════════════════╕')
-                print('│        Game Over 😱        │')
+                print('│        Game Over         │')
                 print('╘════════════════════════════╛\n')
                 pause = input()
                 array_choose = []
                 break
 
-            if counter_win >= len(word) :
+            if counter_win >= len(word)
+ :
                 os.system("clear")
                 print('\n╒════════════════════════════╕')
-                print('│        You win  👏👏👏     │')
+                print('│        You win              │')
                 print('╘════════════════════════════╛\n')
                 pause = input()
                 break
