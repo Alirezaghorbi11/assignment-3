06711597eb599792d82f2593038da8730356217a
Alirezaghorbi11 <92166188+Alirezaghorbi11@users.noreply.github.com>
2021-11-07 15:20:23 +0330

    Update and rename chess.py to snake.py

---
+++ snake.py
@@ -0,0 +1,12 @@
+def Show_snake(l):
+    print('\n\n')
+    for i in range(l):
+        print('⬛'+'⬜', end='')
+            
+
+
+lengths = int(input('Plz Enter the len of snake :\n\t==> '))
+
+
+Show_snake(lengths  )
+print('\n\n')
